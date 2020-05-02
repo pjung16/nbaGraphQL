d0ac2cc5d8526a96d90ac2811ea1852213a71afc
Philip Jung <philip.jung.98@gmail.com>
2020-05-01 22:03:21 -0400

    added build folder

---
+++ client/build/precache-manifest.44000e505a398737199c5f96c09a98ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46f49e846fbc00357af6bc7dbfb20d4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c83e46c9c42e30ef62af",
+    "url": "/static/css/main.7e313e05.chunk.css"
+  },
+  {
+    "revision": "452676a842e2f6ee73af",
+    "url": "/static/js/2.22646041.chunk.js"
+  },
+  {
+    "revision": "c83e46c9c42e30ef62af",
+    "url": "/static/js/main.96ca1c71.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
